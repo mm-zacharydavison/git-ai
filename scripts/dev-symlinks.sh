1a1332017045b0ab575c007de3f35b9c20455661
Sasha Varlamov <sasha@sashavarlamov.com>
2025-10-02 07:19:43 -0400

    add dev env setup script

---
+++ scripts/dev-symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+mkdir -p ./target/gitwrap/bin
+
+echo "Creating symlinks from target/debug to gitwrap"
+ln -sf $(pwd)/target/debug/git-ai $(pwd)/target/gitwrap/bin/git
+ln -sf $(pwd)/target/debug/git-ai $(pwd)/target/gitwrap/bin/git-ai
+
+echo "In your shell profile,"
+echo "1. Remove any artifacts from running 'install.sh'"
+echo "2. Remove any aliases to git"
+echo "3. Add the following to your shell profile:"
+echo "# git-ai local dev"
+echo "export PATH=\"/Users/svarlamov/projects/git-ai/target/gitwrap/bin:\$PATH\""
