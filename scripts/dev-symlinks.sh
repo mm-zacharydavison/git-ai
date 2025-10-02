8da4c47fe0e9fbea29dec12e272c788d5b1c5c67
Sasha Varlamov <sasha@sashavarlamov.com>
2025-10-02 07:25:42 -0400

    fix script typo

--- scripts/dev-symlinks.sh
+++ scripts/dev-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -euo pipefail
 
 mkdir -p ./target/gitwrap/bin
 
@@ -13,4 +13,4 @@ echo "1. Remove any artifacts from running 'install.sh'"
 echo "2. Remove any aliases to git"
 echo "3. Add the following to your shell profile:"
 echo "# git-ai local dev"
-echo "export PATH=\"/Users/svarlamov/projects/git-ai/target/gitwrap/bin:\$PATH\""
+echo "export PATH=\"$(pwd)/target/gitwrap/bin:\$PATH\""
