4c74ffc83d77163b8db3d815bdf1f0b70322ce60
Aidan Cunniffe <acunniffe@gmail.com>
2025-10-28 16:30:36 -0400

    pull and fetch

--- src/commands/git_handlers.rs
+++ src/commands/git_handlers.rs
@@ -60,6 +60,7 @@ pub struct CommandHooksContext {
     pub rebase_original_head: Option<String>,
     pub _rebase_onto: Option<String>,
     pub push_authorship_handle: Option<std::thread::JoinHandle<()>>,
+    pub fetch_authorship_handle: Option<std::thread::JoinHandle<()>>,
 }
 
 pub fn handle_git(args: &[String]) {
@@ -76,6 +77,7 @@ pub fn handle_git(args: &[String]) {
         rebase_original_head: None,
         _rebase_onto: None,
         push_authorship_handle: None,
+        fetch_authorship_handle: None,
     };
 
     let parsed_args = parse_git_cli_args(args);
@@ -160,6 +162,10 @@ fn run_pre_command_hooks(
             command_hooks_context.push_authorship_handle =
                 push_hooks::push_pre_command_hook(parsed_args, repository);
         }
+        Some("fetch") | Some("pull") => {
+            command_hooks_context.fetch_authorship_handle =
+                fetch_hooks::fetch_pull_pre_command_hook(parsed_args, repository);
+        }
         _ => {}
     }
 }
@@ -178,12 +184,12 @@ fn run_post_command_hooks(
             repository,
             command_hooks_context,
         ),
-        Some("fetch") => {
-            fetch_hooks::fetch_pull_post_command_hook(repository, parsed_args, exit_status)
-        }
-        Some("pull") => {
-            fetch_hooks::fetch_pull_post_command_hook(repository, parsed_args, exit_status)
-        }
+        Some("fetch") | Some("pull") => fetch_hooks::fetch_pull_post_command_hook(
+            repository,
+            parsed_args,
+            exit_status,
+            command_hooks_context,
+        ),
         Some("push") => push_hooks::push_post_command_hook(
             repository,
             parsed_args,
